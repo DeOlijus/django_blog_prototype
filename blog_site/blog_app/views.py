5b3f4c33225ac110639606a04068f10475996311
toastmuseum <Deolijus@gmail.com>
2020-08-05 15:43:39 -0400

    Create views for About Page and for Viewing/Modifying Posts

--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView, DetailView)
+from django.views.generic import (CreateView, UpdateView, DeleteView) #CRUD Views
+from django.urls import reverse_lazy
+from blog_app.models import Post, Comment
+from blog_app.forms import PostForm, CommentForm
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(CreateView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_detail.html'
+    form_class = PostForm
+
+    model = Post
+
+class UpdatePostView(CreateView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_detail.html'
+    form_class = PostForm
+
+    model = Post
+
+class DeletePostView(DeleteView, LoginRequiredMixin):
+    login_url = '/login/'
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(ListView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
