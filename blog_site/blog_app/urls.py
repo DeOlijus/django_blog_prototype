c5fbf4bc4ed1b75d9c80ab09cfeff33b91e10645
toastmuseum <Deolijus@gmail.com>
2020-08-06 15:40:54 -0400

    Connect function based views to url patterns

--- blog_site/blog_app/urls.py
+++ blog_site/blog_app/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     re_path(r'^post/(?P<pk>\d+)/update/$', views.UpdatePostView.as_view(), name = 'post_update'),
     re_path(r'^post/(?P<pk>\d+)/delete/$', views.DeletePostView.as_view(), name = 'post_delete'),
     re_path(r'^drafts/$', views.DraftListView.as_view(), name = 'post_draft_list'),
+    re_path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name = 'add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$', views.approve_comment_on_post, name = 'approve_comment_on_post'),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$', views.remove_comment_on_post, name = 'remove_comment_on_post'),
+    re_path(r'^post/(?P<pk>\d+)/publish/$', views.publish_post, name='publish_post'),
 
 ]
