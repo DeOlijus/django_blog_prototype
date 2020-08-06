e3e90af92945f4e263df03e19ce47cbf74c28f54
toastmuseum <Deolijus@gmail.com>
2020-08-06 15:38:33 -0400

    add function based views to publish posts and to modify comments on posts

--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView, ListView, DetailView)
 from django.views.generic import (CreateView, UpdateView, DeleteView) #CRUD Views
@@ -48,3 +50,42 @@ class DraftListView(ListView, LoginRequiredMixin):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+
+@login_required
+def publish_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+
+    context_dict = {'form':form}
+    return render(request, 'blog_app/comment_form.html', context_dict)
+
+
+@login_required
+def approve_comment_on_post(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def remove_comment_on_post(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
